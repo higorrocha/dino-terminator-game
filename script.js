dd894e8042ad40fe797e671c564e5df4d8958d65
Higor Rocha <you@example.com>
2020-08-23 20:33:47 -0300

    Making corrections in JS and add setTimeout function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -11,8 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
-
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -50,6 +49,10 @@ function createCatus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Fim de jogo
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="fim-de-jogo">Exterminado!</h1>';
         } else{
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
